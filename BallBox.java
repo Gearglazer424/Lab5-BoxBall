cbbcd76f47b54b7942258b13833c7c0051d71e45
Gearglazer424 <G00218336@W213-08.academic.raritanval.edu>
2015-10-19 20:40:26 -0400

    created new class

---
+++ BallBox.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class BallBox here.
+ * 
+ * @author (Jeremy Hixson) 
+ * @version (10/19/15)
+ */
+public class BallBox
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class BallBox
+     */
+    public BallBox()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+}
